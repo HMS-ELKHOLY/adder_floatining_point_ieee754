da2e8563e37e10b928f7be479de8c9adfc1b4728
HMS-ELKHOLY <hussien.m.elkholy@gmail.com>
2019-04-06 07:22:11 +0200

    "needs testing"

---
+++ simulation/modelsim/rtl_work/adder_floating_point/_primary.vhd
@@ -0,0 +1,13 @@
+library verilog;
+use verilog.vl_types.all;
+entity adder_floating_point is
+    port(
+        operand_normalized_ieee_a: in     vl_logic_vector(31 downto 0);
+        operand_normalized_ieee_b: in     vl_logic_vector(31 downto 0);
+        final_sum       : out    vl_logic_vector(31 downto 0);
+        carry_of_fraction: out    vl_logic;
+        finish          : out    vl_logic;
+        overflow        : out    vl_logic;
+        underflow       : out    vl_logic
+    );
+end adder_floating_point;
