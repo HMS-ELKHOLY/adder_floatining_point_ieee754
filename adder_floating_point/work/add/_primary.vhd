002793d6a7ff8b90d8a54c1eb0778a1926669582
HMS-ELKHOLY <hussien.m.elkholy@gmail.com>
2019-06-13 16:46:19 +0200

    delete files

--- adder_floating_point/work/add/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity add is
-    port(
-        a               : in     vl_logic_vector(3 downto 0);
-        b               : in     vl_logic_vector(3 downto 0);
-        c               : out    vl_logic_vector(3 downto 0)
-    );
-end add;
--- adder_floating_point/work/t/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity t is
-end t;
